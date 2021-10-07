01ac0f926c2a9a875019527f9727302e14ad6df7
ComputerG1 <montemu4@go.stockton.edu>
2021-10-07 15:58:01 -0400

    update to master

--- HW1/Replace.java
+++ HW1/Replace.java
@@ -7,14 +7,44 @@ package HW1;
 // HELLO
 // 01234
 public class Replace {
-    public String charReplace(String s, int x, char z) {
-        return null;
-        
 
+public static void main(String[] args) { // Main method
 
+    String appleBro = "Andrew loves programing, not\n"; //assigned string with words and new line.
 
+    System.out.println("Before: " + appleBro); //printed beofre so I can see what the string looks like. 
 
-        
-    }
+    appleBro = appleBro.replaceAll("not", "").replaceAll(",", "."); //used replace all and did a double replace. 
+
+    System.out.println("After: " + appleBro); //printed after to see changes. 
+
+    String classExample = "I MAKE BREAD, 2, T\n"; //assigned string with words and new line.
+
+    System.out.println("Before: "+ classExample);//printed beofre so I can see what the string looks like.
+
+    classExample = classExample.replaceAll("MAKE","TAKE").replaceAll("2, T","").replaceAll(",", ""); //used replace all and did a triple replace.
+
+    System.out.println("After: " + classExample);//printed after to see changes.
+
+    String classExample2 = "HELLO\n"; //assigned string with words and new line.
+
+    System.out.println("Before: " + classExample2);//printed beofre so I can see what the string looks like. 
+
+    classExample2 = classExample2.replaceAll("HELLO", "01234");//used replace all and did a single replace. 
+
+    System.out.println("After: " + classExample2);//printed after to see changes.
+
     
+
+    
+
+
+
+
+
 }
+
+
+}
+    
+
