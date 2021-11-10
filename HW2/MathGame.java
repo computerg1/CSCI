3327650b1054a138bf8036999e4e6eb1cf0803d9
ComputerG1 <montemu4@go.stockton.edu>
2021-11-10 13:16:58 -0500

    update to master

---
+++ HW2/MathGame.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+//This is my attempt at having a cool menu driven looptastic program.
+public class MathGame {
+
+    public static void main(String[] args) {
+
+        System.out.println("HW2: Begin!");
+
+        //Menu Coding
+        System.out.println("Welcome to Math Games! \nPlease Select from the following:");
+        System.out.println("1- Hypotenuse Calculator.");
+        System.out.println("2- Grade Calculator.");
+        System.out.println("3- Tip Calculator And Bill Splitter.");
+        System.out.println("4- Exit");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please select \"1\", \"2\", or \"3\"");
+        int menu = sc.nextInt();
+        while (menu != 4) {
+            if (menu < 1 || menu > 3) {
+                System.out.println("Select \"1\", \"2\", or \"3\"");
+                menu = sc.nextInt();
+            }
+
+            else if(menu == 1){
+                //Pythagorean Theorem Object
+                String trianglePythagorean = Triangle.Pythagorean();
+                System.out.println(trianglePythagorean);
+            }
+            
+            else if(menu == 2){
+                //Grading Object
+                String gradingGrades = Grading.Grades();
+                System.out.println(gradingGrades);
+            }
+            
+            else if(menu == 3){
+                //Tip + Bill Object
+                String tipCalculator = Tips.Bill();
+                System.out.println(tipCalculator);
+            }
+            else if(menu == 4){
+                System.exit(4);
+            }
+            
+        }
+        //close all scanners in each object??
+        sc.close();
+    }
+    
+}
---
+++ HW2/Triangle.java
@@ -0,0 +1,23 @@
+
+
+import java.util.Scanner;
+
+public class Triangle {
+
+    public static String Pythagorean() {
+
+        Scanner angle = new Scanner(System.in);
+        double side1, side2, hypotenuse;
+        System.out.println("Welcome to the Pythagorean Theorem Calculator!");
+        System.out.println("Please Enter A Value For Side 1: "); 
+        side1 = angle.nextDouble();
+        System.out.println("Please Enter A Value for Side 2: ");
+        side2 = angle.nextDouble();
+        hypotenuse = Math.sqrt((side1*side1)+(side2*side2));
+        System.out.println("The length of the Hypotenuse is: " + hypotenuse);
+
+        
+        return "Quick Maths";
+    }
+    
+}
