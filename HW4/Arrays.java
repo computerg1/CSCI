297b1b1c454fa9467370cc8992e594ade550b60d
computerg1 <montemu4@go.stockton.edu>
2021-12-01 12:22:19 -0500

    update to homeowrk

--- HW4/Arrays.java
+++ HW4/Arrays.java
@@ -38,8 +38,6 @@ public class Arrays {
 
         sc.close();
 
-
-
     }
 
     // Create a method that will brute force a password EX.
@@ -63,6 +61,41 @@ public class Arrays {
         'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
         '!', '@', '#', '$', '%', '^', '&', '*', '(', ')','<','>','/','?'};
     
+       int n = password.lenght(); 
+
+       int m = 66; 
+
+       int index = 0; 
+
+       for(int i = 0; i < n; i++){
+
+            for(int j = 0; j < m; j++){
+
+                if()
+
+
+
+
+            }
+
+
+
+
+
+       }
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
         return answer; 
     }
 
