c86bee222b45250cea944202e500c36a12e2196e
computerg1 <montemu4@go.stockton.edu>
2021-12-01 12:32:15 -0500

    update to homworkl

--- HW4/Arrays.java
+++ HW4/Arrays.java
@@ -111,27 +111,25 @@ public class Arrays {
 
             for(int j = 0; j < n - i - 1; j++){
 
-                
+                if(nums[j] > nums [j + 1]){
 
+                    int temp = nums[j + 1]; 
 
+                    nums[j] = nums[j + 1]; 
+
+                    nums[j + 1] = temp; 
+
+                }
 
             }
 
-
-
         }
 
-
-
-
-
-
+        sortedArray = nums; 
 
         return sortedArray;
+
     }
-
-
-
 
     public static void main(String[] args) {
 
